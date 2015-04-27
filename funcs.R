85c4e3ac51510fcb56e8d324cf10f84bf9e088f1
Nick Pullen <nick.pullen@jic.ac.uk>
2015-04-27 15:22:41 +0100

    potentially useful fn for including Rmd files

--- funcs.R
+++ funcs.R
@@ -73,3 +73,15 @@ emptyPlot = function() {
   return(p)
 }
 
+## from https://groups.google.com/d/msg/shiny-discuss/ObgFdmusyJM/3p2Hf_KjaqgJ
+includeRmd <- function(path){
+  if (!require(knitr))
+    stop("knitr package is not installed")
+  if (!require(markdown))
+    stop("Markdown package is not installed")
+  shiny:::dependsOnFile(path)
+  contents = paste(readLines(path, warn = FALSE), collapse = '\n')
+  html <- knitr::knit2html(text = contents, fragment.only = TRUE)
+  Encoding(html) <- 'UTF-8'
+  return(HTML(html))
+}
